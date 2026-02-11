3c205dcb79981770cef80a073719237c85d0bdb5
mandy <changlin1010@gmail.com>
2026-02-11 14:43:39 +0800

    update project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/clio_vue3/', // ← GitHub Pages repository 名稱
+  base: '/mandy-0010/', // ← GitHub Pages repository 名稱
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
